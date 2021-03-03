7dcb60bde64c6ac8c7ca8311377a99e361088670
Billy Hao <billyhao12@gmail.com>
2021-03-03 15:48:11 -0800

    Added two test cases. Formatting.

--- arrays/easy/twoSum.js
+++ arrays/easy/twoSum.js
@@ -1,33 +1,26 @@
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
-*/
 
-/*
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
 
+Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
-*/
 
-/*
 Constraints:
-
 2 <= nums.length <= 105
 -109 <= nums[i] <= 109
 -109 <= target <= 109
+
 Only one valid answer exists.
 */
 
@@ -48,4 +41,7 @@ const twoSum = function(nums, target) {
     }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
+// Test cases
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3,2,4], 6)); // [1, 2]
+console.log(twoSum([3,3], 6)); // [0, 1]
